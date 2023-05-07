b827e5696581725d1c8b1c7dd635d2bdc70f6dd7
Mening <mening1085@gmail.com>
2023-05-07 23:38:30 +0700

    add git.md

---
+++ GIT.md
@@ -0,0 +1,11 @@
+echo "# tool-sys" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mening1085/tool-sys.git
+git push -u origin main
+
+git remote add origin https://github.com/mening1085/tool-sys.git
+git branch -M main
+git push -u origin main
--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -9,7 +9,7 @@
             </div>
 
             <form action="/" method="post">
-                <div class="flex items-center gap-3">
+                <d iv class="flex items-center gap-3">
                     <div class="w-full">
                         <input
                             class="appearance-none block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -35,7 +35,7 @@
 
 
 
-                </div>
+                </d>
             </form>
         </div>
 
